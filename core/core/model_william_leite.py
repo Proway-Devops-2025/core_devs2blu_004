a019c5da8e2650c891e1f1b089f268fe8000d41e
WilliamLeite02 <william.a.antunesleite@gmail.com>
2025-11-10 19:44:55 -0300

    feat: create model william leite

---
+++ core/core/model_william_leite.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, DateTime
+from core.core.DATABASE.base_class import Base  # O path do import já deve estar correto
+
+class WilliamLeite(Base):
+    __tablename__ = "william_leite"
+
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
