59903c708d3de1a89b0cb8637f51eb5aabbe8e18
Max-leal <Max13leal@gmail.com>
2025-11-10 21:17:51 -0300

    feat: add nome, cep, rua to model

--- core/core/model_max_augusto.py
+++ core/core/model_max_augusto.py
@@ -7,4 +7,8 @@ class MaxAugusto(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
-    #update
+    
+    # Novas propriedades
+    nome = Column(String(100))
+    cep = Column(String(9))
+    rua = Column(String(200))
