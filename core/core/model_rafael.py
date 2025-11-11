8de917f181043fc9fd2a371253f410735fc0cd3f
WilliamLeite02 <william.a.antunesleite@gmail.com>
2025-11-10 21:35:27 -0300

    feat: new changes in the model_rafael.py

--- core/core/model_rafael.py
+++ core/core/model_rafael.py
@@ -7,3 +7,8 @@ class RafaelDemarch(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+  # Novas propriedades
+    nome = Column(String(100))
+    cep = Column(String(9))
+    rua = Column(String(200))
