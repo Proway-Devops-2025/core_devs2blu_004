a71ced833370721ba0a6b8fdc4bfcf0f21cbf8ba
PedroZanella <pedro_zanela@hotmail.com>
2025-11-12 19:49:14 -0300

    adicionada nova função na feat core floripa

--- core/core/feat-002-core-team-floripa.py
+++ core/core/feat-002-core-team-floripa.py
@@ -1,1 +1,11 @@
 core-giovanni(par1, par2);
+
+Fuction two Predo
+def order_pizza(size, toppings):
+    print(f"🍕 Preparing your {size} pizza with:")
+    for topping in toppings:
+        print(f" - {topping}")
+    print("Your pizza will be ready in 20 minutes. Enjoy!")
+
+# Exemplo de uso
+order_pizza("large", ["pepperoni", "mushrooms", "extra cheese"])
