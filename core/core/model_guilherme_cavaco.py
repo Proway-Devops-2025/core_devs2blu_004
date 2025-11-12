e2b1f96b719ec39fe4d461c42b0351da086ecbab
Guilherme Cavaco <guihcavaco@gmail.com>
2025-11-12 19:48:37 -0300

    feat/002-equipe-blumenau-1: Documentação no código

--- core/core/model_guilherme_cavaco.py
+++ core/core/model_guilherme_cavaco.py
@@ -8,8 +8,8 @@ class GuilhermeCavaco(Base):
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
 
-numeros = [1, 2, 3, 4]
-quadrados = []
-for n in numeros:
-    quadrados.append(n ** 2)
-print(quadrados)
+numeros = [1, 2, 3, 4] ## Numeross a serem elevados ao quadrado
+quadrados = [] ## Lista para armazenar os quadrados
+for n in numeros:  ## Loop através dos números
+    quadrados.append(n ** 2) ## Adiciona o quadrado do número à lista
+print(quadrados) ## Imprime a lista de quadrados
