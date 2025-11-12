e80054d615e1876fb8f7c24b07e09d291558bd94
PedroZanella <pedro_zanela@hotmail.com>
2025-11-12 20:10:44 -0300

    exclusão de model duplicado

--- core/model_Pedro_Zanella.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from sqlalchemy import Column, Integer, DateTime
-from core.core.DATABASE.base_class import Base  # ajuste se necessário ao path real
-
-class MariaSilva(Base):
-    __tablename__ = "pedro_zanella"
-
-    id = Column(Integer, primary_key=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
